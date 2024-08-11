70dfd7af7917aff91989e8e5c46c74153a09bc64
Lastbaks <missouri@inbox.ru>
2024-08-11 10:46:35 +0300

    KTB-05 вывод начального меню в цикле while(true)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -12,11 +12,34 @@ fun main() {
     val dictionary = mutableListOf<Word>()
 
     val wordsFile = File("words.txt")
-    val lines: List<String> =wordsFile.readLines()
+    val lines: List<String> = wordsFile.readLines()
     for (line in lines) {
         val line = line.split("|")
-        val word = Word(original = line[0], translate = line[1], correctAnswersCount = line[2].toInt()?: 0)
+        val word = Word(original = line[0], translate = line[1], correctAnswersCount = line[2].toInt() ?: 0)
         dictionary.add(word)
     }
-    println(dictionary)
+
+    while (true) {
+        println("Меню:")
+        println("1 - Учить слова")
+        println("2 - Статистика")
+        println("0 - Выход")
+
+        val input = readln()
+
+        when (input) {
+            "1" -> {
+                println(1)
+            }
+
+            "2" -> {
+                println(2)
+            }
+
+            "0" -> return
+            else -> {
+                println("Введено некорректное число")
+            }
+        }
+    }
 }
