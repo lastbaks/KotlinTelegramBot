0d97f9a7c281e9419648ee471e6a143052eb2510
Lastbaks <missouri@inbox.ru>
2024-08-13 21:23:00 +0300

    KTB-08 - проверка ответа, выход в меню

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,12 +4,11 @@ import java.io.File
 
 fun main() {
     val dictionary = mutableListOf<Word>()
-
     val wordsFile = File("words.txt")
     val lines: List<String> = wordsFile.readLines()
     for (line in lines) {
-        val line = line.split("|")
-        val word = Word(original = line[0], translate = line[1], correctAnswersCount = line[2].toInt() ?: 0)
+        val item = line.split("|")
+        val word = Word(original = item[0], translate = item[1], correctAnswersCount = item[2].toInt())
         dictionary.add(word)
     }
 
@@ -23,27 +22,38 @@ fun main() {
         when (input) {
             "1" -> {
                 while (true) {
-                    println("Меню изучения слов.")
-                    var nonLearnedWords = dictionary.filter { it.correctAnswersCount <= 3 }
-                    if (nonLearnedWords.size == 0) {
+                    print("Слово: ")
+                    val nonLearnedWords = dictionary.filter { it.correctAnswersCount <= 3 }
+                    if (nonLearnedWords.isEmpty()) {
                         println("Все слова выучены")
                         return
                     }
                     val answers = nonLearnedWords.shuffled().take(4)
                     val question = answers.shuffled().take(1)
                     var answer: Int
-                    println("Слово для изучения:")
+
                     question.forEach {
                         println(it.original)
                     }
                     println("Введите номер с правильным вариантом перевода:")
-                    answers.forEachIndexed { index, value -> println("$index - ${value.translate}") }
-                    answer = readln().toInt()
+                    answers.forEachIndexed { index, value -> println("${index + 1} - ${value.translate}") }
+                    println("0 - Выход в главное меню")
+
+                    answer = (readln().toInt())
+                    if (answer == 0) break
+                    if (answers[answer - 1].translate == question[0].translate) {
+
+                        dictionary.forEach {
+                            if (it.translate == answers[answer].translate) {
+                                it.correctAnswersCount++
+                            }
+                        }
+                        saveDictionary(dictionary, wordsFile)
+                    }
                 }
             }
 
             "2" -> {
-                println(2)
                 // Печать статистики по выученым словам
                 val totalWordsCount = dictionary.size
                 val correctAnswers = dictionary.filter { it.correctAnswersCount > 3 }.size
@@ -57,5 +67,12 @@ fun main() {
                 println("Введено некорректное число")
             }
         }
+    }
+}
+
+fun saveDictionary(dictionary: MutableList<Word>,  wordsFile: File) {
+    wordsFile.writeText("")
+    dictionary.forEach {
+        wordsFile.appendText("${ it.original }|${it.translate}|${it.correctAnswersCount}\n")
     }
 }
